e7b9d869ab6c1942ae4d9cc7b16f541729e590e3
LolaPaglino <52178062@ivt.edu.ar>
2026-06-08 11:17:25 -0300

    edite el app.js

--- TrabajoPractico5/app.js
+++ TrabajoPractico5/app.js
@@ -3,7 +3,7 @@ let resta = 0;
 let multiplicacion = 0;
 let division = 0;
 
-let numero1 =  2;
+let numero1 = 2;
 let numero2 = 4;
 
 suma = numero1+numero2;
@@ -26,3 +26,15 @@ console.log("el alumno es:"+ nombrealumno+" "+ apellido +" y tiene "+ edad)
  let precio2 = 5
  let precio3 = 20
  let total = precio1 + precio2 + precio3
+ console.log("el total es: "+ total)
+
+ let comidafavorita =" milanesa";
+ let colorfavorito = "azul";
+ let numerofavorito = 20
+
+ console.log("mi comida favorita es la "+ comidafavorita + " me gusta el color "+ colorfavorito +"y mi numero favorito es "+ numerofavorito )
+
+ let pais ="Argentina"
+ console.log(pais)
+ pais = "Brasil"
+ console.log(pais)
