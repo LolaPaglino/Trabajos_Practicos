a8debbb39ff60abce56430f9165e783cee96414e
alumno <52178062@ivt.edu.ar>
2026-07-02 09:17:55 -0300

    mi trabajo practico6

---
+++ TP6/app.js
@@ -0,0 +1,4 @@
+let boton1 =document.querySelector("#boton1")
+let boton1 =document.querySelector("#boton2")
+let boton1 =document.querySelector("#boton3")
+let boton1 =document.querySelector("#boton4")
